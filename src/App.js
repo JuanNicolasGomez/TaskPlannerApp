45550f65aace825b68ee26575f4b5fa56e2a0765
JuanNicolasGomez <nicolas.gm243@gmail.com>
2019-02-16 21:48:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo3.png';
 import './App.css';
+import {Login} from './component/Login.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <h1 className="App-title">Task Planner</h1>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <Login/>
         </header>
       </div>
     );
